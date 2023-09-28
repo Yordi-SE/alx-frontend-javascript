14b3cb56962f78adb7cb3c6dd50888d8ac17ec54
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-09-28 19:26:53 +0000

    js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2])
+  return Promise.all([promise1, promise2])
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })

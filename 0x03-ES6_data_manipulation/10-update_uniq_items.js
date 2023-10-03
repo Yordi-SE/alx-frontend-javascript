f5c086429e532310655ffb760a514d33a98a396d
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-10-03 15:48:26 +0000

    js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,8 @@
 export default function updateUniqueItems(map) {
   const map2 = map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of map2) {
     if (value === 1) {
       map2.set(key, 100);
